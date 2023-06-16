ab98527b841702c61f732a1f16fd8689d8db2a0f
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-06-16 18:37:48 -0400

    troubleshooting displaying index issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 'id', value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
@@ -27,9 +27,9 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  // console.log('result.value', result);
+  console.log('result.value', result);
   return result;
 };
 
