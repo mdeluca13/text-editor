3752cc7cfcb63b5491f300148683000c54b1d8f3
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-06-16 19:54:57 -0400

    fixed display index issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,16 +1,17 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const initdb = async () => {
+ await openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('jate store already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('jate store created');
     },
   });
+};
 
 export const putDb = async (content) => {
   console.log('PUT to the database');
@@ -19,18 +20,18 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data saved to the jate database', result);
 };
 
 export const getDb = async () => {
-  console.log('GET all from the database');
+  console.log('GET from the jate database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
