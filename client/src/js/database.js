1650b4c5adbe49a95f8ec81bf716263197e9e7f4
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-06-16 17:40:24 -0400

    fixed start issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,12 +12,12 @@ const initdb = async () =>
     },
   });
 
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, value: content });
+  const request = store.put({ id: 'id', value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
@@ -29,7 +29,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
+  // console.log('result.value', result);
   return result;
 };
 
